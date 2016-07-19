133dd417646c47c906750b08ca5fde53dd05cd1d
ercain03 <ercain3@gmail.com>
2016-07-19 15:20:59 -0700

    added files to cowsay

---
+++ lab-tre/test/cowsay_test.js
@@ -0,0 +1,10 @@
+const expect = require('chai').expect;
+const assert = require('assert');
+const cowsay = require('../lib/cowsay');
+
+describe('what the cow says', function(){
+  it('should say what the user inputed', function(){
+    expect(cowsay('tre')).to.eql('moo cow says tre');
+    assert.deepEqual(cowsay('tre'), cowsay('tre'));
+  });
+});
